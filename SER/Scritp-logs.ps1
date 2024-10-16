90419e98a361b94f9ac9e6ed4c3d84ef16d0bff2
Noé <131699900+N0EV@users.noreply.github.com>
2024-10-17 00:06:25 +0200

    Update Scritp-logs.ps1

--- SER/Scritp-logs.ps1
+++ SER/Scritp-logs.ps1
@@ -5,8 +5,10 @@ $smtpServer = "smtp.tuproovedordecorreo.com"
 # A quien envia el correo
 $to = "uncorreo@loqusea.es"
 $subject = "Notificacion de $($env:computername)"
+
 # Recoge la información del ultimo log que se ha echo
-$evento = Get-Eventing -LogName "Security" -Newest 1
+$evento = Get-Eventlog -LogName "Security" -Newest 1
+
 # Esto es lo que aparece en el correo que se envia recogiendo datos con las variables del sistema windows
 $body = @"
 Evento a revisar en $($evento.MachineName)
@@ -19,7 +21,8 @@ Texto: $($evento.Message)
 
 # Va a ir al smtp Server para autentificarse y enviar el correo
 $smtpClient = New-Object System.Net.Mail.SmtpClient($smtpServer, 587)
-$smtpClient.EnableSs1 = $true
+$smtpClient.EnableSsl = $true
 $smtpClient.Credentials = New-Object System.Net.NetworkCredential("Usuario(tuCorreo)", "Contraseña(La que generes para permitir que este script entre en el correo para enviarlo por ti)")
 $smtpClient.Send($from, $to, $subject, $body)
+
 # Esta ultima seccion tienes que poner tu correo y una contraseña que hayas generado para la aplicación en especifico
